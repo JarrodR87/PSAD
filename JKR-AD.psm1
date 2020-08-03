90e2bd236c8bd27bcface623961d13821383f434
Jarrod Rother <Jarrod.Rother@Knowles.com>
2020-08-03 15:28:53 -0400

    Fixed bad Dash

--- JKR-AD.psm1
+++ JKR-AD.psm1
@@ -988,45 +988,6 @@ function Get-InsecureLDAPBindCount {
 
 } #FUNCTION
 
-function Get-ADUserGroupsFriendlyNames {
-    <#
-        .SYNOPSIS
-            Gets a list of Group Names as Friendly Names instead of DN's
-        .DESCRIPTION
-            Pulls the Users Group List and then converts the list to Friendly Names
-        .PARAMETER Identity
-            User whose groups to search
-        .PARAMETER Domain
-            Optional - Uses Current Domain if not specified
-        .EXAMPLE
-            Get-ADUserGroupsFriendlyNames -Identity User1 -Domain test.com
-        .EXAMPLE
-            Get-ADUserGroupsFriendlyNames -Identity User2
-    #>
-    [CmdletBinding()]
-    Param(
-        [Parameter(Mandatory = $true)]$Identity,
-        [Parameter()]$Domain
-    ) 
-    BEGIN { 
-        if ($NULL -eq $Domain) {
-            $Domain = (Get-ADDomain).DNSRoot
-        }
-    } #BEGIN
-
-    PROCESS {
-            $Groups = (Get-ADUser $Identity –Properties MemberOf -Server $Domain).MemberOf
-            $GroupFriendlyNames = foreach ($Group in $Groups) {
-                (Get-ADGroup $Group -Server $Domain).name 
-            }
-    } #PROCESS
-
-    END { 
-        $GroupFriendlyNames
-    } #END
-
-} #FUNCTION
-
 function Get-InsecureLDAPBinds {
     <#
         .SYNOPSIS
@@ -1118,6 +1079,45 @@ function Copy-ADUserGroups {
 
     END { 
 
+    } #END
+
+} #FUNCTION
+
+function Get-ADUserGroupsFriendlyNames {
+    <#
+        .SYNOPSIS
+            Gets a list of Group Names as Friendly Names instead of DN's
+        .DESCRIPTION
+            Pulls the Users Group List and then converts the list to Friendly Names
+        .PARAMETER Identity
+            User whose groups to search
+        .PARAMETER Domain
+            Optional - Uses Current Domain if not specified
+        .EXAMPLE
+            Get-ADUserGroupsFriendlyNames -Identity User1 -Domain test.com
+        .EXAMPLE
+            Get-ADUserGroupsFriendlyNames -Identity User2
+    #>
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $true)]$Identity,
+        [Parameter()]$Domain
+    ) 
+    BEGIN { 
+        if ($NULL -eq $Domain) {
+            $Domain = (Get-ADDomain).DNSRoot
+        }
+    } #BEGIN
+
+    PROCESS {
+        $Groups = ((Get-ADUser $Identity -Properties MemberOf -Server $Domain).MemberOf)
+        $GroupFriendlyNames = foreach ($Group in $Groups) {
+            (Get-ADGroup $Group -Server $Domain).name 
+        }
+    } #PROCESS
+
+    END { 
+        $GroupFriendlyNames
     } #END
 
 } #FUNCTION
