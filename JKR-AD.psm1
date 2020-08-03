746b6a6c65f4a4d1f66c38b2a0277d358beee3eb
Rother <Jarrod.Rother@knowles.com>
2020-08-03 12:08:38 -0400

    8-3-2020

--- JKR-AD.psm1
+++ JKR-AD.psm1
@@ -929,3 +929,195 @@ function Get-ADServerNoStaticDNS {
     } #END
 
 } #FUNCTION
+
+function Get-InsecureLDAPBindCount {
+    <#
+        .SYNOPSIS
+            Gets count of Insecure LDAP Bind Events
+        .DESCRIPTION
+            Required Audit logging be turned up to see these Events generated
+        .PARAMETER Domain
+            Domain to search. It will use the Current Domain if none is specified
+        .PARAMETER Days
+            Specified number of Days in the past to check. Defaults to 14
+        .EXAMPLE
+            Get-InsecureLDAPBindCount -Domain Test.com -Days 3
+        .EXAMPLE
+            Get-InsecureLDAPBindCount
+    #>
+    [CmdletBinding()]
+    Param(
+        [Parameter()]$Domain,
+        [Parameter()]$Days
+    ) 
+    BEGIN { 
+        if ($NULL -eq $Domain) {
+            $Domain = (Get-ADDomain).DNSRoot
+        }
+
+        if ($NULL -eq $Days) {
+            $Days = '14'
+        }
+
+        $DCs = (Get-ADDomainController -Filter * -Server $Domain).Name
+        $InsecureLDAPBinds = @()
+    } #BEGIN
+
+    PROCESS {
+        foreach ($DC in $DCs) {
+            $Event2886 = Get-WinEvent -ComputerName $DC -FilterHashtable @{Logname = 'Directory Service'; Id = 2886; StartTime = (get-date).AddDays(-$Days) }
+            $Event2887 = Get-WinEvent -ComputerName $DC -FilterHashtable @{Logname = 'Directory Service'; Id = 2887; StartTime = (get-date).AddDays(-$Days) }
+            $Event2888 = Get-WinEvent -ComputerName $DC -FilterHashtable @{Logname = 'Directory Service'; Id = 2888; StartTime = (get-date).AddDays(-$Days) }
+            $Event2889 = Get-WinEvent -ComputerName $DC -FilterHashtable @{Logname = 'Directory Service'; Id = 2889; StartTime = (get-date).AddDays(-$Days) }
+
+            $Row = New-Object PSObject
+            $Row | Add-Member -MemberType noteproperty -Name "DC" -Value $DC
+            $Row | Add-Member -MemberType noteproperty -Name "Event2886Count" -Value $Event2886.Count
+            $Row | Add-Member -MemberType noteproperty -Name "Event2887Count" -Value $Event2887.Count
+            $Row | Add-Member -MemberType noteproperty -Name "Event2888Count" -Value $Event2888.Count
+            $Row | Add-Member -MemberType noteproperty -Name "Event2889Count" -Value $Event2889.Count
+
+            $InsecureLDAPBinds += $Row
+        }
+
+    } #PROCESS
+
+    END { 
+        $InsecureLDAPBinds
+    } #END
+
+} #FUNCTION
+
+function Get-ADUserGroupsFriendlyNames {
+    <#
+        .SYNOPSIS
+            Gets a list of Group Names as Friendly Names instead of DN's
+        .DESCRIPTION
+            Pulls the Users Group List and then converts the list to Friendly Names
+        .PARAMETER Identity
+            User whose groups to search
+        .PARAMETER Domain
+            Optional - Uses Current Domain if not specified
+        .EXAMPLE
+            Get-ADUserGroupsFriendlyNames -Identity User1 -Domain test.com
+        .EXAMPLE
+            Get-ADUserGroupsFriendlyNames -Identity User2
+    #>
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $true)]$Identity,
+        [Parameter()]$Domain
+    ) 
+    BEGIN { 
+        if ($NULL -eq $Domain) {
+            $Domain = (Get-ADDomain).DNSRoot
+        }
+    } #BEGIN
+
+    PROCESS {
+            $Groups = (Get-ADUser $Identity –Properties MemberOf -Server $Domain).MemberOf
+            $GroupFriendlyNames = foreach ($Group in $Groups) {
+                (Get-ADGroup $Group -Server $Domain).name 
+            }
+    } #PROCESS
+
+    END { 
+        $GroupFriendlyNames
+    } #END
+
+} #FUNCTION
+
+function Get-InsecureLDAPBinds {
+    <#
+        .SYNOPSIS
+            Queries DC's for Insecure Binds and what is doing them
+        .DESCRIPTION
+            Requires Directory Service Logging to be turned up for these events to be generated, but then queries them for what is doing Insecure Binds
+        .PARAMETER Domain
+            Domain to search. It will use the Current Domain if none is specified
+        .PARAMETER Days
+            Specified number of Days in the past to check. Defaults to 14
+        .EXAMPLE
+            C
+    #>
+    [CmdletBinding()]
+    Param(
+        [Parameter()]$Domain,
+        [Parameter()]$Days
+    ) 
+    BEGIN { 
+        if ($NULL -eq $Domain) {
+            $Domain = (Get-ADDomain).DNSRoot
+        }
+
+        if ($NULL -eq $Days) {
+            $Days = '14'
+        }
+
+        $DCs = (Get-ADDomainController -Filter * -Server $Domain).Name
+        $InsecureBinds = @()
+    } #BEGIN
+
+    PROCESS {
+        Foreach ($DC in $DCs) {
+            $Event2889 = Get-WinEvent -ComputerName $DC -FilterHashtable @{Logname = 'Directory Service'; Id = 2889; StartTime = (get-date).AddDays(-$Days) }
+
+            foreach ($Event in $Event2889) {
+
+                $Row = New-Object PSObject
+                $Row | Add-Member -MemberType noteproperty -Name "User" -Value $Event.Properties.Value[1]
+                $Row | Add-Member -MemberType noteproperty -Name "IP" -Value $Event.Properties.Value[0]
+                $Row | Add-Member -MemberType noteproperty -Name "Timecreated" -Value $Event.TimeCreated
+
+                $InsecureBinds += $Row
+            }
+
+            $DC = $Null
+        }
+
+    } #PROCESS
+
+    END { 
+        $InsecureBinds
+    } #END
+
+} #FUNCTION
+
+function Copy-ADUserGroups {
+    <#
+        .SYNOPSIS
+            Copies one AD Users Groups to another User, skipping duplicates
+        .DESCRIPTION
+            Queries list of Users Groups and copies them to the destination user. Cannot currently copy remote-domain groups
+        .PARAMETER Domain
+            Domain to search. It will use the Current Domain if none is specified
+        .EXAMPLE
+            Copy-ADUserGroups -SourceUser User1 -DestUser User2 -Domain Test.com
+        .EXAMPLE
+            Copy-ADUserGroups -SourceUser User1 -DestUser User2
+    #>
+    [CmdletBinding()]
+    Param(
+        [Parameter()]$SourceUser,
+        [Parameter()]$DestUser,
+        [Parameter()]$Domain
+    ) 
+    BEGIN { 
+        if ($NULL -eq $Domain) {
+            $Domain = (Get-ADDomain).DNSRoot
+        }
+
+        $Source = Get-ADUser -Identity $SourceUser -Properties MemberOf -Server $Domain
+        $Dest = Get-ADUser -Identity $DestUser -Properties MemberOf -Server $Domain
+
+    } #BEGIN
+
+    PROCESS {
+        $Source.MemberOf | Where-Object { $Dest.MemberOf -notcontains $_ } |  Add-ADGroupMember -Members $Dest
+    } #PROCESS
+
+    END { 
+
+    } #END
+
+} #FUNCTION
